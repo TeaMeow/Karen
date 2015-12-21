759eaac1c856e8598f3fff890a5d2e650935fc6b
Yami Odymel <yamiodymel@gmail.com>
2015-12-21 16:37:29 +0800

    Create structure.php

---
+++ test/structure.php
@@ -0,0 +1,29 @@
+<?php
+
+return 
+[
+    '0' => [
+                'welcome_title'       => '歡迎來到這裡！',
+                'welcome_description' => '這裡是充滿歡樂的地方。'
+           ],
+    
+    
+    'homepage' => [
+                      'welcome_title' => '歡迎來到首頁！',
+                      'user_count'    => '目前有 %s 位使用者。'
+                  ]
+
+];
+
+?>
+
+
+
+_e('welcome_title');
+_e('welcome_description');
+
+
+_e('homepage.welcome_title');
+
+
+
