9eddaf11a30ff695e11425c310887d4593047523
Yami Odymel <yamiodymel@gmail.com>
2016-02-02 13:04:23 +0000

    New test for textDomain().

--- test/test.php
+++ test/test.php
@@ -10,7 +10,11 @@ include '../src/karen.php';
 
 Karen::initialize('../src/languages/');
 
+
+_e('not_logged_in');
+
+Karen::textDomain('template');
+
+_e('welcome_title');
+
 ?>
-
-
-<div><?= __('welcome_title'); ?></div>
